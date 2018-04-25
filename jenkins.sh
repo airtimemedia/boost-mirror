fed3fc68b1bf62ebb78a60cc11b6a1c4fb391aaf
Tom Hughes <tom@airtime.com>
2018-04-25 10:01:50 -0700

    CAR-957: Use forked version of boost::test 1.67.0

--- jenkins.sh
+++ jenkins.sh
@@ -12,43 +12,6 @@ rm -rf boost-release
 # prep boost
 git submodule update --init --recursive
 VERSIONS="`./versions.sh`"  # get all library versions
-
-# CAR-957: patch boost-test to disable "predef-check" feature not available
-# in boost-1.57 (seems to have been added in "predef" lib in 1.58.0
-# The check is only disabling a warning, so it's safe to disable
-pushd libs/test
-patch -p1 --ignore-whitespace << EOF
-diff --git a/build/Jamfile.v2 b/build/Jamfile.v2
-index 092e0193..3de6b26b 100644
---- a/build/Jamfile.v2
-+++ b/build/Jamfile.v2
-@@ -4,9 +4,9 @@
- #
- #  See http://www.boost.org/libs/test for the library home page.
-
--import ../../predef/check/predef
--    : check
--    : predef-check ;
-+#import ../../predef/check/predef
-+#    : check
-+#    : predef-check ;
-
- project boost/test
-     : source-location ../src
-@@ -16,7 +16,7 @@ project boost/test
-                    <link>shared,<toolset>msvc:<cxxflags>-wd4275
-                    <toolset>msvc:<cxxflags>-wd4671
-                    <toolset>msvc:<cxxflags>-wd4673
--                   [ predef-check "BOOST_COMP_GNUC >= 4.3.0" : : <cxxflags>-Wno-variadic-macros ]
-+                   #[ predef-check "BOOST_COMP_GNUC >= 4.3.0" : : <cxxflags>-Wno-variadic-macros ]
-                    <toolset>clang:<cxxflags>-Wno-c99-extensions
-                    <toolset>clang:<cxxflags>-Wno-variadic-macros
-                    <warnings>all
-
-EOF
-
-popd
-
 ./bootstrap.sh
 ./b2 headers
 
